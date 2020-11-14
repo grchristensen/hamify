f008c872f5d95236c91882c29b4557c7c8777581
chrisgrob <grchr4000@gmail.com>
2020-11-14 15:05:22 -0800

    Add post/get request parsing for transform

---
+++ dnasbtransform/SimpleSpamTransformer.py
@@ -0,0 +1,8 @@
+class SimpleSpamTransformer:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def transform(spam):
+        return spam
+
---
+++ dnasbtransform/test_simple_spam_transformer.py
@@ -0,0 +1,10 @@
+from SimpleSpamTransformer import SimpleSpamTransformer
+
+
+def test_transform_should_not_return_empty_string():
+    transformer = SimpleSpamTransformer()
+
+    transformed_spam = transformer.transform("FREE AVOCADOS")
+
+    assert isinstance(transformed_spam, str)
+    assert transformed_spam != ""
